3c87634eb789c09ac7f86d531c52a92e2aaabcfb
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-14 12:21:29 +0530

    Add population data for cities

---
+++ scripts/3.customer-table-data-population.sql
@@ -0,0 +1,8 @@
+INSERT INTO cities (name, country, population, area)
+VALUES ('Tokyo', 'Japan', 37400068, 2188);
+INSERT INTO cities (name, country, population, area)
+VALUES ('Delhi', 'India', 28514000, 573);
+INSERT INTO cities (name, country, population, area)
+VALUES ('Shanghai', 'China', 25582000, 6340);
+INSERT INTO cities (name, country, population, area)
+VALUES ('Sao Paulo', 'Brazil', 21650000, 1521);
